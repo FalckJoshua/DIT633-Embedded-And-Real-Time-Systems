7e4ce798b9ea2c0da6fdb97a2e9a258cf4ceb75f
William Andersson <willand@student.chalmers.se>
2024-02-06 14:05:02 +0100

    wp2: exercise 4

--- wp2/exercise4/code.c
+++ wp2/exercise4/code.c
@@ -1,34 +1,30 @@
 #include <ctype.h>   // Used for isdigit()
 #include <stdio.h>   // Used for printf(), scanf()
-#include <stdlib.h>  // Used for atoi()
+#include <stdlib.h>  // Used for atoi(), malloc(), free()
 
 // Enum to store the input values order for the input arguments / packing order
 enum INPUT {
-    ENGINE_ON = 1,
+    ENGINE_ON,
     GEAR_POS,
     KEY_POS,
     BRAKE1,
     BREAK2
 };
 
-enum MAX_RANGE {
-    ENGINE_ON_MAX = 1,
-    GEAR_POS_MAX = 4,
-    KEY_POS_MAX = 2,
-    BRAKE1_MAX = 1,
-    BREAK2_MAX = 1
-};
+// Constants
+const char* INPUT_NAMES[] = {"ENGINE_ON", "GEAR_POS", "KEY_POS", "BRAKE1", "BRAKE2"};  // Array to store the input names
+const int MAX_RANGE[] = {1, 4, 2, 1, 1};                                               // Array to store the max range for each input
+const int BIT_SIZE[] = {1, 3, 2, 1, 1};                                                // Array to store the bit size for each input
 
-enum BIT_SIZE {
-    ENGINE_ON_SIZE = 1,
-    GEAR_POS_SIZE = 3,
-    KEY_POS_SIZE = 2,
-    BRAKE1_SIZE = 1,
-    BREAK2_SIZE = 1
-};
+// Global variables
+int input[5];  // Array to store the input values
+
+// Function declarations
+char* bit8_to_hex(int binary);
 
 /**
- * This program
+ * This program takes 5 arguments, one for each enum 'INPUT' value, checking validity and then packing them into a single 8-bit value.
+ * Afterwards, it prints the packed value in hexadecimal format.
  *
  * Work Packages 2: Exercise 4 - BITPACKING
  * DIT633 Development of embedded and Real-Time Systems
@@ -37,17 +33,64 @@ enum BIT_SIZE {
  * Group: 1
  * Submission code: TODO
  **/
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
+    // Check if sufficient amount of arguments have been passed
     if (argc != 6) {
         printf("Invalid number of arguments, expected 5, got %d\n", argc - 1);
-        return 1;
+        return 1;  // Exit program
     }
 
-    for (int i = 1; i < argc; i++) {
-        if (!isdigit(argv[i][0]) && atoi(argv[i]) < 0) {
-            printf("Error: Argument %d is not a number or less than 0\n", i);
-            return 1;
+    // Loop through all arguments and check if they are valid and assign them to the input array
+    for (int i = 0; i < argc - 1; i++) {
+        int arg_nr = i + 1;             // argv starts at 1, since 0 is the program name
+        char* curr_arg = argv[arg_nr];  // Get the current argument
+
+        // Check if the current argument is a number and greater than 0
+        if (!isdigit(curr_arg[0])) {
+            printf("Error: Argument %d is not a number\n", i);
+            return 1;  // Exit program
         }
+
+        // Convert the current argument to an integer
+        int value = atoi(curr_arg);
+
+        // Check if the current argument is within the valid range
+        if (value < 0 || value > MAX_RANGE[i]) {
+            printf("Error: Argument %d (for %s) is out of range, %d - %d, given: %s\n", arg_nr, INPUT_NAMES[i], 0, MAX_RANGE[i], curr_arg);
+            return 1;  // Exit program
+        }
+
+        // Assign the current argument to the input array
+        input[i] = value;
     }
-    for
+
+    int packed = 0;                      // Variable to store the packed value
+    for (int i = 0; i < 5; i++) {        // Loop through all the input values
+        packed = packed << BIT_SIZE[i];  // Shift the packed value to the left by the bit size of the current input
+        packed = packed | input[i];      // Add the current input value to the packed value
+    }
+
+    char* packed_hex = bit8_to_hex(packed);  // Convert the packed value to hexadecimal format
+    printf("0x%s\n", packed_hex);            // Print the packed value in hexadecimal format'
+
+    free(packed_hex);  // Free the memory allocated for the packed hexadecimal value
+
+    return 0;  // Exit program
+}
+
+// Function to convert an 8-bit binary value to a hexadecimal string
+char* bit8_to_hex(int binary) {
+    char* hex = malloc(3 * sizeof(char));  // Allocate memory for 2 characters and a null terminator
+    if (!hex) return NULL;                 // Check if memory allocation was successful
+
+    char hex_values[] = "0123456789ABCDEF";
+
+    for (int i = 1; i >= 0; i--) {             // Loop through the 2 characters in the hexadecimal value
+        hex[i] = hex_values[binary & 0b1111];  // Get the last 4 bits of the binary value and convert it to a hexadecimal character
+        binary = binary >> 4;                  // Shift the binary value to the right by 4 bits
+    }
+
+    hex[2] = '\0';  // Null-terminate the string
+
+    return hex;  // Return the hexadecimal string
 }
--- wp2/exercise4/decode.c
+++ wp2/exercise4/decode.c
@@ -1,5 +1,27 @@
+#include <ctype.h>   // Used for isdigit(), isxdigit()
+#include <stdio.h>   // Used for printf(), scanf()
+#include <stdlib.h>  // Used for atoi(), malloc(), free()
+#include <string.h>  // Used for strlen()
+
+// Enum to store the input values order for the input arguments / packing order
+enum INPUT {
+    ENGINE_ON,
+    GEAR_POS,
+    KEY_POS,
+    BRAKE1,
+    BREAK2
+};
+
+// Constants
+const char* INPUT_NAMES[] = {"ENGINE_ON", "GEAR_POS", "KEY_POS", "BRAKE1", "BRAKE2"};  // Array to store the input names
+const int MAX_RANGE[] = {1, 4, 2, 1, 1};                                               // Array to store the max range for each input
+const int BIT_SIZE[] = {1, 3, 2, 1, 1};                                                // Array to store the bit size for each input
+
+// Function declarations
+void decode(int packed);
+
 /**
- * This program 
+ * This program takes 1 argument, a hex value, and decodes it into the 5 input values, checking validity and then printing them.
  *
  * Work Packages 2: Exercise 4 - BITPACKING
  * DIT633 Development of embedded and Real-Time Systems
@@ -8,5 +30,53 @@
  * Group: 1
  * Submission code: TODO
  **/
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc != 2) {  // Check if the number of arguments is not 2
+        printf("Error: Invalid number of arguments, expected 1, got %d\n", argc - 1);
+        return 1;  // Exit program
+    }
+
+    int length = strlen(argv[1]);  // Get the length of the provided argument
+    if (length != 2) {             // Check to see if the provided argument is more than 2 characters
+        printf("Error: Too many characters, expected 2, got: %d\n", length);
+        return 1;  // Exit program
+    }
+
+    for (int i = 0; i < 2; i++) {     // Loop through all the characters in the provided argument
+        if (!isxdigit(argv[1][i])) {  // Check if the character is not a valid hex number
+            printf("Error: Argument is not a valid hex number\n");
+            return 1;  // Exit program
+        }
+    }
+
+    int packed;                      // Variable to store the packed value
+    sscanf(argv[1], "%x", &packed);  // Convert the hex value to an integer
+
+    decode(packed);  // Call function to decode the binary value. If valid 'packed', print the value of each enum 'INPUT' value
+
+    return 0;  // Exit program, indicate success
+}
+
+void decode(int packed) {
+    int unpacked[5];                                      // Array to store the the binary values separately for each input, in the order of the enum INPUT
+    for (int i = 4; i >= 0; i--) {                        // Loop through all the input values, in reverse order
+        unpacked[i] = packed & ((1 << BIT_SIZE[i]) - 1);  // Mask the packed value with the bit size
+        packed = packed >> BIT_SIZE[i];                   // Shift the packed value to the right by the bit size
+    }
+
+    for (int i = 0; i < 5; i++) {          // Loop through all the input values
+        if (unpacked[i] > MAX_RANGE[i]) {  // Check if the unpacked value is out of range
+            printf("Error: Unpacked value for %s is out of range (%d - %d), got: %d\n", INPUT_NAMES[i], 0, MAX_RANGE[i], unpacked[i]);
+            return;  // Exit function
+        }
+    }
+
+    // Print the unpacked values
+    printf(" ___________________\n");                           // Make adorable box
+    printf("|Name          Value|\n");                          // ----||----
+    printf("|-------------------|\n");                          // ----||----
+    for (int i = 0; i < 5; i++) {                               // Loop through all the input values
+        printf("|%-15s %-3d|\n", INPUT_NAMES[i], unpacked[i]);  // print the input name and value
+    }                                                           //
+    printf("|___________________|\n");                          // Close the cool box
 }
