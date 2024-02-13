e7c774ff5d0a4a4a3a7f6525a2a9a26caf84e998
William Andersson <willand@student.chalmers.se>
2024-02-13 14:59:11 +0100

    wp3: exercise 5 & 6

--- wp3/exercise5/main.c
+++ wp3/exercise5/main.c
@@ -1,16 +1,19 @@
 // Includes
-#include <stdio.h>  // Used for printf
-#include <stdlib.h>
-#include <time.h>  // Used for srand
+#include <stdio.h>   // Used for printf
+#include <stdlib.h>  // Used for rand
+#include <time.h>    // Used for srand
+
+// Constants
+#define MAX 10  // Define the constant MAX to be used as the size of the array
 
 // Function declarations
-void generate_numbers(int *arr);
-void print_all(int *arr);
-
-const int MAX = 10;
+void generate_numbers(int *arr);  // Function to generate random numbers for each element in the array
+void print_all(int *arr);         // Function to print all the elements in the array
 
 /**
- * This program
+ * This program generates an array of size 'MAX' with random numbers between 1 and 99.
+ * Afterwards, it prints random information about the array and its elements.
+ * Lastly, it prints all the elements in the array and the value of each element multiplied by 2, using pointers.
  *
  * Work Packages 4: Exercise 5 - POINTERS AND ARRAYS
  * DIT633 Development of embedded and Real-Time Systems
@@ -21,27 +24,33 @@ const int MAX = 10;
  **/
 
 int main(int argc, char *argv[]) {
-    int arr[MAX];
-    srand(time(NULL));
-    generate_numbers(arr);
+    int arr[MAX];           // Initialize an array with MAX integer elements
+    srand(time(NULL));      // Seed the random number generator with the current time
+    generate_numbers(arr);  // Call the function to generate random numbers for each element in the array
+
+    int integer_size = sizeof(int);
 
     printf("The value of the address of the array (pointer) is: %p\n", arr);
     printf("First integer in the array is (array[0]): %d\n", *arr);
     printf("The last integer in the array is: %d\n", arr[MAX - 1]);
-    printf("The size of an integer (number of bytes) is: %lu\n", sizeof(int));
-    printf("The size of the whole array in bytes is: %lu\n", sizeof(arr));
+    printf("The size of an integer (number of bytes) is: %d\n", integer_size);
+    printf("The size of the whole array in bytes is: %d\n", integer_size * MAX);
 
-    print_all(arr);
+    print_all(arr);  // Call the function to print all the elements in the array
 }
 
+// Function to generate random numbers for each element in the array
 void generate_numbers(int *arr) {
-    for (int i = 0; i < MAX; i++) {
-        arr[i] = rand() % 99 + 1;
+    for (int i = 0; i < MAX; i++) {  // Loop through all the elements in the array
+        arr[i] = rand() % 99 + 1;    // Generate and assign a random number between 1 and 99
     }
 }
 
+// Function to print all the elements in the array
 void print_all(int *arr) {
-    for (int i = 0; i < MAX; i++) {
-        printf("arr[%d] = %d. The value 2x is: %d\n", i, *(arr + i), *(arr + i) * 2);
+    for (int i = 0; i < MAX; i++) {  // Loop through all the elements in the array
+
+        // Print the current element (i) in the array and the value of the element by using an incremented pointer (arr + i)
+        printf("arr[%d] = %3d. The value 2x is: %3d\n", i, *(arr + i), *(arr + i) * 2);
     }
 }
--- wp3/exercise6/main.c
+++ wp3/exercise6/main.c
@@ -1,5 +1,16 @@
+#include <ctype.h>   // Used for atoi()
+#include <stdio.h>   // Used for printf()
+#include <stdlib.h>  // Used for malloc()
+#include <string.h>  // Used for strcpy(), strlen()
+
+// Function declarations
+int exit_and_free(char* input, char* err_message);  // Function to exit the program and free the memory
+void copy_string(char* input, int free_mem);        // Function to copy the input string
+char* copy_String_with_strcpy(char* string);        // Function to copy the input string with strcpy()
+char* copy_String_with_loop(char* string);          // Function to copy the input string with a loop
+
 /**
- * This program 
+ * This program
  *
  * Work Packages 4: Exercise 6 - ARRAYS AND FILES
  * DIT633 Development of embedded and Real-Time Systems
@@ -8,3 +19,100 @@
  * Group: 1
  * Submission code: TODO
  **/
+int main(int argc, char* argv[]) {
+    if (argc > 1) {
+        if (strlen(argv[1]) > 20) {
+            printf("The input is too long.\n");
+            return 1;
+        }
+
+        copy_string(argv[1], 0);
+
+    }
+
+    else {
+        char* input = (char*)malloc(20 * sizeof(char));
+        // If there are no command-line arguments, read a line from stdin
+        if (fgets(input, 20, stdin) == NULL) {
+            exit_and_free(input, "Error reading input.\n");
+        }
+
+        // After reading from the file...
+        fclose(stdin);
+
+        // Reopen stdin to read from the terminal
+        stdin = fopen("/dev/tty", "r");
+        if (stdin == NULL) {
+            perror("Error reopening stdin");
+            exit(EXIT_FAILURE);
+        }
+
+        copy_string(input, 1);
+    }
+
+    return 0;
+}
+
+int exit_and_free(char* input, char* err_message) {
+    printf("%s", err_message);
+    free(input);
+    return 1;
+}
+
+void copy_string(char* input, int free_mem) {
+    printf("Input: %s\n", input);
+
+    printf("Copy with strcpy or loop? (1/2): ");
+    char choice[2];
+    scanf("%s", choice);
+    while (atoi(choice) != 1 && atoi(choice) != 2) {
+        printf("Invalid input. Please enter 1 or 2: ");
+        scanf("%s", choice);
+    }
+
+    char* copy;
+
+    if (atoi(choice) == 1) {
+        copy = copy_String_with_strcpy(input);
+        if (copy == NULL) {
+            exit_and_free(input, "Error allocating memory for copy.\n");
+        }
+        printf("Copy with strcpy: %s\n", copy);
+    }
+
+    else {
+        copy = copy_String_with_loop(input);
+        if (copy == NULL) {
+            exit_and_free(input, "Error allocating memory for copy.\n");
+        }
+        printf("Copy with loop: %s\n", copy);
+    }
+
+    if (free_mem) {
+        free(input);
+    }
+    free(copy);
+}
+
+char* copy_String_with_strcpy(char* string) {
+    char* copy = (char*)malloc(strlen(string) * sizeof(char));
+    if (copy == NULL) {
+        return NULL;
+    }
+
+    strcpy(copy, string);
+    return copy;
+}
+
+char* copy_String_with_loop(char* string) {
+    char* copy = (char*)malloc(strlen(string) * sizeof(char));
+    if (copy == NULL) {
+        return NULL;
+    }
+
+    for (int i = 0; i < strlen(string); i++) {
+        copy[i] = string[i];
+    }
+
+    return copy;
+}
