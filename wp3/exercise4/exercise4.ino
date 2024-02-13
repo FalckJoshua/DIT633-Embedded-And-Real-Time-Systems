28850de8312de0d57995d0eeaf57158db5e857ff
William Andersson <willand@student.chalmers.se>
2024-02-13 13:38:40 +0100

    wp3: exercise 4 & 5

--- wp3/exercise4/exercise4.ino
+++ wp3/exercise4/exercise4.ino
@@ -1,5 +1,5 @@
 /**
- * This program 
+ * This program scans a 4x4 keypad and prints the pressed key to the serial monitor, using the Keypad library.
  *
  * Work Packages 3: Exercise 4 - KEYBOARD SCANNING V 2.0
  * DIT633 Development of embedded and Real-Time Systems
@@ -8,3 +8,32 @@
  * Group: 1
  * Submission code: TODO
  **/
+
+// Includes
+#include <Keypad.h>
+
+// Matrix for characters for the 4x4 keypad
+char char_matrix[4][4] = {
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}};
+
+const byte ROWS = 4;
+const byte COLS = 4;
+byte row_pins[ROWS] = {11, 10, 9, 8};  // Pins for the rows
+byte col_pins[COLS] = {7, 6, 5, 4};    // Pins for the columns
+
+Keypad keypad = Keypad(makeKeymap(char_matrix), row_pins, col_pins, ROWS, COLS);
+
+void setup() {
+    Serial.begin(9600);
+}
+
+void loop() {
+    char key = keypad.getKey();
+
+    if (key) {
+        Serial.println(key);
+    }
+}
--- wp3/exercise5/main.c
+++ wp3/exercise5/main.c
@@ -1,5 +1,16 @@
+// Includes
+#include <stdio.h>  // Used for printf
+#include <stdlib.h>
+#include <time.h>  // Used for srand
+
+// Function declarations
+void generate_numbers(int *arr);
+void print_all(int *arr);
+
+const int MAX = 10;
+
 /**
- * This program 
+ * This program
  *
  * Work Packages 4: Exercise 5 - POINTERS AND ARRAYS
  * DIT633 Development of embedded and Real-Time Systems
@@ -8,3 +19,29 @@
  * Group: 1
  * Submission code: TODO
  **/
+
+int main(int argc, char *argv[]) {
+    int arr[MAX];
+    srand(time(NULL));
+    generate_numbers(arr);
+
+    printf("The value of the address of the array (pointer) is: %p\n", arr);
+    printf("First integer in the array is (array[0]): %d\n", *arr);
+    printf("The last integer in the array is: %d\n", arr[MAX - 1]);
+    printf("The size of an integer (number of bytes) is: %lu\n", sizeof(int));
+    printf("The size of the whole array in bytes is: %lu\n", sizeof(arr));
+
+    print_all(arr);
+}
+
+void generate_numbers(int *arr) {
+    for (int i = 0; i < MAX; i++) {
+        arr[i] = rand() % 99 + 1;
+    }
+}
+
+void print_all(int *arr) {
+    for (int i = 0; i < MAX; i++) {
+        printf("arr[%d] = %d. The value 2x is: %d\n", i, *(arr + i), *(arr + i) * 2);
+    }
+}
