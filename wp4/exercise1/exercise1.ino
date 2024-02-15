85ca759b6d48b526a74fe7713d516ca9906c0adf
William Andersson <willand@student.chalmers.se>
2024-02-15 16:34:04 +0100

    wp4: exercise 1 & 2

---
+++ wp4/exercise1/exercise1.ino
@@ -0,0 +1,108 @@
+/**
+ * This program turns on LEDs based on the temperature read from a sensor using interrupts on a timer.
+ *
+ * Work Packages 4: Exercise 1 - INTERRUPTS ON TIMER
+ * DIT633 Development of embedded and Real-Time Systems
+ *
+ * Authors: William Andersson, Joshua Chiu Falck, Adam Faundez Laurokari, 2024
+ * Group: 1
+ * Submission code: TODO
+ **/
+
+// LED pins
+#define LED1_PIN 12
+#define LED2_PIN 11
+#define LED3_PIN 10
+#define LED4_PIN 9
+#define LED5_PIN 8
+
+// LED temperature thresholds
+const int LED_THRESHOLDS[] = {0, 10, 20, 30, 40};
+
+// Temperature sensor pin
+#define TEMP_PIN A0
+
+// Taken from course material:
+// https://www.tinkercad.com/things/hqsDje5Qtdv?sharecode=CVIAWPHOUN6TuzFbgOV2ztOiFeXxdcmmXmHZHbqg57o
+//
+// Timer definitions
+#define PRESCALER 1024        // Used for dictating speed of timer according to (timer speed (Hz)) = (Arduino clock speed (16MHz)) / prescaler
+#define INTERRUPT_FREQ 1      // Define interrupt frequency
+#define CLOCK_SPEED 16000000  // Clock speed of the Arduino Uno
+#define TIMER1_MAX 65535      // Max counter value for timer1
+#define TIMER1_LOW 257        // Lowest counter value for timer1
+
+// Global variables for timer
+int timer1 = 0;
+int iCounter = 0;
+
+void setup() {
+    // Initialize LED pins
+    pinMode(LED1_PIN, OUTPUT);
+    pinMode(LED2_PIN, OUTPUT);
+    pinMode(LED3_PIN, OUTPUT);
+    pinMode(LED4_PIN, OUTPUT);
+    pinMode(LED5_PIN, OUTPUT);
+
+    pinMode(TEMP_PIN, INPUT);  // Initialize temperature sensor pin
+
+    setup_timer_1();     // Setup timer
+    Serial.begin(9600);  // Begin serial
+}
+
+void loop() {
+}
+
+// Taken from course material:
+// https://www.tinkercad.com/things/hqsDje5Qtdv?sharecode=CVIAWPHOUN6TuzFbgOV2ztOiFeXxdcmmXmHZHbqg57o
+//
+// Original comment:
+/*
+ * This function sets up timer1 on an Arduino Uno
+ * Built on top of: https://www.instructables.com/Arduino-Timer-Interrupts/
+ * Sets timer1 interrupt at 1Hz = 1 second = 1 cycle per second
+ */
+void setup_timer_1() {
+    cli();  // stop interrupts
+
+    TCCR1A = 0;  // set entire TCCR1A register to 0
+    TCCR1B = 0;  // same for TCCR1B
+    TCNT1 = 0;   // initialize counter value to 0
+
+    // calculate at which point to stop the counter and set to int:
+    timer1 = (CLOCK_SPEED / (PRESCALER * INTERRUPT_FREQ)) - 1;
+    // timer1 = (CLOCK_SPEED / (INTERRUPT_FREQ)) - 1;
+    //  timer1 = 16 000 000 / 65535
+
+    if (timer1 >= TIMER1_MAX) {                                                            // if value went above max value
+        timer1 = TIMER1_MAX;                                                               // set timer to max allowed value
+        Serial.println((String) "timer1 was set too high and was changed to: " + timer1);  // notify user
+    } else if (timer1 <= TIMER1_LOW) {                                                     // if value went below lowest value
+        timer1 = TIMER1_LOW;                                                               // set timer to max allowed value
+        Serial.println((String) "timer1 was set too low and was changed to: " + timer1);   // notify user
+    }
+
+    OCR1A = timer1;                       // set compare match register for 1hz increments
+    TCCR1B |= (1 << WGM12);               // turn on CTC mode
+    TCCR1B |= (1 << CS12) | (1 << CS10);  // set CS10 and CS12 bits for 1024 prescaler
+    TIMSK1 |= (1 << OCIE1A);              // enable timer compare interrupt
+
+    sei();  // allow interrupts
+}
+
+// Interrupt service routine for timer1 compare match A
+ISR(TIMER1_COMPA_vect) {
+    cli();  // Disable interrupts
+
+    int temp = map(((analogRead(TEMP_PIN) - 20) * 3.04), 0, 1023, -40, 125);  // Read temperature from sensor
+
+    for (int i = 0; i < 5; i++) {  // Iterate over the LED pins
+
+        // Turn on the LED if the temperature is above the threshold
+        // 'LED1_PIN - i' is used to iterate over the LED pins, first LED is highest PIN (12), last LED is lowest PIN (8)
+        // Ternary operator is used to set the LED to HIGH if the temperature is above the threshold, otherwise LOW
+        digitalWrite(LED1_PIN - i, temp >= LED_THRESHOLDS[i] ? HIGH : LOW);
+    }
+
+    sei();  // Allow interrupts
+}
---
+++ wp4/exercise2/exercise2.ino
@@ -0,0 +1,106 @@
+/**
+ * This program
+ *
+ * Work Packages 4: Exercise 1 - INTERRUPTS ON TIMER
+ * DIT633 Development of embedded and Real-Time Systems
+ *
+ * Authors: William Andersson, Joshua Chiu Falck, Adam Faundez Laurokari, 2024
+ * Group: 1
+ * Submission code: TODO
+ **/
+
+#include <Servo.h>  // Include the servo library
+
+// Define the pin for the servo
+#define SERVO_PIN A1
+
+// Inspiration taken from course material:
+// https://www.tinkercad.com/things/hqsDje5Qtdv?sharecode=CVIAWPHOUN6TuzFbgOV2ztOiFeXxdcmmXmHZHbqg57o
+//
+// Changes made:
+// - Use timer2 instead of timer1
+//
+// Timer definitions
+#define PRESCALER 1024        // Used for dictating speed of timer according to (timer speed (Hz)) = (Arduino clock speed (16MHz)) / prescaler
+#define INTERRUPT_FREQ 1      // Define interrupt frequency
+#define CLOCK_SPEED 16000000  // Clock speed of the Arduino Uno
+#define TIMER2_MAX 255        // Max counter value for timer0
+#define TIMER2_LOW 0          // Lowest counter value for timer0
+
+// Global variables for timer
+int timer2 = 0;                  // Timer counter
+int iCounter = 0;                // Counter for the number of times the interrupt has been called
+int servoPosition = 0;           // Current position of the servo
+unsigned long last_time_called;  // Last time the interrupt was called
+
+// Create a servo object
+Servo servoBase;
+
+void setup() {
+    servoBase.attach(SERVO_PIN);  // Attach the servo to pin 'SERVO_PIN'
+    servoBase.write(0);           // Set the initial position of the servo to 0 degrees
+
+    setup_timer_2();              // Setup timer
+    Serial.begin(115200);         // Begin serial
+    last_time_called = millis();  // Set the last time the interrupt was called to the current time
+}
+
+void loop() {
+    // Do nothing
+}
+
+// Inspiration taken from course material:
+// https://www.tinkercad.com/things/hqsDje5Qtdv?sharecode=CVIAWPHOUN6TuzFbgOV2ztOiFeXxdcmmXmHZHbqg57o
+//
+// Changes made:
+// - Use timer2 instead of timer1
+//
+// Original comment:
+/*
+ * This function sets up timer1 on an Arduino Uno
+ * Built on top of: https://www.instructables.com/Arduino-Timer-Interrupts/
+ * Sets timer1 interrupt at 1Hz = 1 second = 1 cycle per second
+ */
+void setup_timer_2() {
+    cli();  // Stop interrupts
+
+    TCCR2A = 0;  // set entire TCCR2A register to 0
+    TCCR2B = 0;  // same for TCCR2B
+    TCNT2 = 0;   // initialize counter value to 0
+
+    OCR2A = 250;                                        // set compare match register for 1hz increments
+    TCCR2A |= (1 << WGM21);                             // turn on CTC mode
+    TCCR2B |= (1 << CS20) | (1 << CS21) | (1 << CS22);  // set CS20, CS21, and CS22 bits for 1024 prescaler
+    TIMSK2 |= (1 << OCIE2A);                            // enable timer compare interrupt
+
+    sei();  // Allow interrupts
+}
+
+// Interrupt service routine for timer1 compare match A
+ISR(TIMER2_COMPA_vect) {
+    cli();                                  // Stop interrupts
+    unsigned long current_time = millis();  // Get the current time
+
+    iCounter++;           // Increment the counter
+    if (iCounter < 64) {  // If the counter is less than 64, return (to ensure that the servo is only updated every 64 interrupts, equals ~1024ms at 16MHz clock speed)
+        return;           // This is to ensure that the servo is only updated every 64 interrupts
+    }
+    iCounter = 0;  // Reset the counter
+
+    servoPosition = (servoPosition + 1) % 360;  // Increment the servo position by 1 degree (0-180 degrees)
+
+    servoBase.write(servoPosition);  // Set the servo to the current position
+
+    // Print the time since the last interrupt was called
+    String message;  // Variable to store the message
+    message = "Called: " + String(current_time - last_time_called) + "ms ago. ";
+    message += "(Current time: " + String(current_time) + " ms, ";
+    message += "Previous call: " + String(last_time_called) + "ms, ";
+    message += "Servo position: " + String(servoPosition) + " degrees)";
+
+    Serial.println(message);  // Print the message
+
+    last_time_called = current_time;  // Update the last time the interrupt was called
+
+    sei();  // Allow interrupts
+}
