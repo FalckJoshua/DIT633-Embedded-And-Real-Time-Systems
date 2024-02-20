de5f434df21b9be3352353742e7fd7e6f3cb2f87
William Andersson <willand@student.chalmers.se>
2024-02-20 17:37:32 +0100

    wp5: exercise 1 & 2

---
+++ wp5/exercise1/exercise1.ino
@@ -0,0 +1,61 @@
+/**
+ * This program blinks an LED and turns on another LED when a button is pressed.
+ *
+ * Work Packages 5: Exercise 1 - SET-UP
+ * DIT633 Development of embedded and Real-Time Systems
+ *
+ * Authors: William Andersson, Joshua Chiu Falck, Adam Faundez Laurokari, 2024
+ * Group: 1
+ * Submission code: TODO
+ **/
+
+// Pin setup
+int led_1_pin = 12;  // pin 12 for the first LED
+int led_2_pin = 7;   // pin 7 for the second LED
+int button_pin = 2;  // pin 2 for the button
+
+// Variables for time tracking
+unsigned long tracker;  // to track the time
+unsigned long current;  // to store the current time
+int timer = 1000;       // delay for the first LED, 1 second
+
+bool led_1_state;  // stores state of the 1st LED, true for ON, false for OFF
+int button_state;  // stores state of the button, HIGH for pressed, LOW for not pressed
+
+// Setup pins and variables
+void setup() {
+    pinMode(button_pin, INPUT);  // set the button pin as input
+    pinMode(led_1_pin, OUTPUT);  // set the first LED pin as output
+    pinMode(led_2_pin, OUTPUT);  // set the second LED pin as output
+
+    led_1_state = false;  // initialize the state of the first LED
+    tracker = millis();   // initialize the tracker
+
+    attachInterrupt(digitalPinToInterrupt(button_pin), on_button_press, CHANGE);  // Attach an interrupt to the button pin
+}
+
+void loop() {
+    // --- First LED - Blinking ---
+    current = millis();                     // get the current time
+    if (current - tracker >= timer) {       // if the delay has passed:
+        if (led_1_state) {                  // if the LED is ON, turn it OFF
+            led_1_state = false;            // update the state of the led to OFF (false)
+            digitalWrite(led_1_pin, LOW);   // turn the LED OFF
+        } else {                            // if the LED is OFF, turn it ON
+            led_1_state = true;             // update the state of the led to ON (true)
+            digitalWrite(led_1_pin, HIGH);  // turn the LED ON
+        }
+        tracker = current;  // update the tracker to the current time
+    }
+}
+
+// Function to handle the button press, called whenever the current state of the button changes
+void on_button_press() {
+    // --- Second LED - Button Control ---
+    button_state = digitalRead(button_pin);  // read the state of the button
+    if (button_state == HIGH) {              // if the button is pressed
+        digitalWrite(led_2_pin, HIGH);       // turn the LED ON
+    } else {                                 // if the button is not pressed
+        digitalWrite(led_2_pin, LOW);        // turn the LED OFF
+    }
+}
---
+++ wp5/exercise2/part2/part2.ino
@@ -0,0 +1,92 @@
+/**
+ * This program reads the temperature and light intensity from sensors and turns on LEDs depending on the values.
+ *
+ * Work Packages 5: Exercise 2 - TEMPERATURE AND LIGHT METER
+ * DIT633 Development of embedded and Real-Time Systems
+ *
+ * Authors: William Andersson, Joshua Chiu Falck, Adam Faundez Laurokari, 2024
+ * Group: 1
+ * Submission code: TODO
+ **/
+
+// Pins for LEDs
+int led_high_temp = 12;    // Declare the output pin for the high temperature LED
+int led_normal_temp = 11;  // Declare the output pin for the normal temperature LED
+int led_low_temp = 10;     // Declare the output pin for the low temperature LED
+
+// Pins for input
+int light_pin = A0;  // Declare the input pin for the light sensor
+int temp_pin = A1;   // Declare the input pin for the temperature sensor
+
+int timer = 1000;  // Delay between loops
+
+const int low_temp_range[4] = {-40, -12, 1, 31};   // Low temp values
+const int high_temp_range[4] = {-13, 0, 30, 125};  // High temp values
+const int light_range[4] = {0, 20, 60, 90};       // Light percentage
+
+int light;
+int temp;
+
+void setup() {
+    Serial.begin(9600);  // Start the serial monitor with a baud rate of 9600
+
+    pinMode(led_high_temp, OUTPUT);    // Set the led_temp pin as output
+    pinMode(led_normal_temp, OUTPUT);  // Set the led_normal pin as output
+    pinMode(led_low_temp, OUTPUT);     // Set the led_temp pin as output
+
+    pinMode(light_pin, INPUT);  // Set the light_pin as input
+    pinMode(temp_pin, INPUT);   // Set the temp_pin as input
+}
+
+void loop() {
+    int light_input = analogRead(light_pin);                   // Read value from light sensor
+    light = map(light_input, 1023, 0, 0, 100);  // Map the value to a percentage
+
+    // Read value from module connected at pin A0
+    int V_input = analogRead(temp_pin);
+    // Calculate thermometer resistance by comparing it to the modules onboard 10K-ohm resistor
+    float R_ohms = 10000 * (1023.0 / (float)V_input - 1.0);
+    // Calculate temperature in Celsius
+    temp = (1.0 / (0.001129148 + (0.000234125 * log(R_ohms)) + 0.0000000876741 * log(R_ohms) * log(R_ohms) * log(R_ohms))) - 273.15;
+
+    Serial.print("Light: ");
+    Serial.print(light);
+    Serial.print("%, Temperature: ");
+    Serial.print(temp);
+    Serial.println("");
+
+    // Depending on current light intensity, set allowed range for temperature
+    if (light <= light_range[0]) {                              // Lowest interval, light <= 0
+        LED_to_turn_on(low_temp_range[0], high_temp_range[0]);  // Call helper function
+    }
+
+    else if (light <= light_range[1]) {                         // Second lowest interval, light <= 20
+        LED_to_turn_on(low_temp_range[1], high_temp_range[1]);  // Call helper function
+    }
+
+    else if (light <= light_range[2]) {                         // Second highest interval, light <= 60
+        LED_to_turn_on(low_temp_range[2], high_temp_range[2]);  // Call helper function
+    }
+
+    else {                                                      // Highest interval, light <= 100
+        LED_to_turn_on(low_temp_range[3], high_temp_range[3]);  // Call helper function
+    }
+
+    delay(timer);  // Delay 'timer' amount of miliseconds, to have a delay between each loop.
+
+    // Turn off all lights for next iteration
+    digitalWrite(led_high_temp, LOW);    // Set the high temperature LED to LOW (turn it off)
+    digitalWrite(led_normal_temp, LOW);  // Set the normal temperature LED to LOW (turn it off)
+    digitalWrite(led_low_temp, LOW);     // Set the low temperature LED to LOW (turn it off)
+}
+
+// Function to decide which LED to turn on, depending on if the current temperature is lower, between, or higher than the acceptable range
+void LED_to_turn_on(int low, int high) {
+    if (temp > high) {                        // If higher than allowed range:
+        digitalWrite(led_high_temp, HIGH);    // Turn on high temperature LED
+    } else if (temp < low) {                  // If lower than allowed range:
+        digitalWrite(led_low_temp, HIGH);     // Turn on low temperature LED
+    } else {                                  // Else, within allowed range:
+        digitalWrite(led_normal_temp, HIGH);  // Turn on acceptable temperature LED
+    }
+}
